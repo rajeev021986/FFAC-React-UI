cba1438b80342476bd763f64ac9e7ca9367e66b6
RajeshZmedios <rajesh.p@zmediostech.com>
2025-01-02 19:41:59 +0530

    audit/fix

--- src/components/AuditTimeLine.jsx
+++ src/components/AuditTimeLine.jsx
@@ -1,30 +1,34 @@
-
 import React, { useEffect } from "react";
 import Timeline from "./Timeline";
 import { Button, Grid } from "@mui/material";
-import RefreshOutlinedIcon from '@mui/icons-material/RefreshOutlined';
+import RefreshOutlinedIcon from "@mui/icons-material/RefreshOutlined";
 import Loader from "./common/Loader/Loader";
 function AuditTimeLine({ auditDetails, reloadDataHandler, loading }) {
   useEffect(() => {
     reloadDataHandler();
-  }, [])
+  }, []);
   return (
-    <>{loading ? <Loader /> : <div>
-      <Grid container justifyContent="flex-end" marginBottom={2}>
-        <Button
-          variant="contained"
-          color="primary"
-          startIcon={<RefreshOutlinedIcon />}
-          onClick={reloadDataHandler}
-          sx={{borderRadius: '17px 18px 18px 17px'}}
-        >
-          Refresh
-        </Button>
-      </Grid>
-      <Timeline data={auditDetails?.body} />
-    </div>}</>
-  )
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div style={{ minHeight: "55vh" }}>
+          <Grid container justifyContent="flex-end" marginBottom={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={<RefreshOutlinedIcon />}
+              onClick={reloadDataHandler}
+              sx={{ borderRadius: "17px 18px 18px 17px" }}
+            >
+              Refresh
+            </Button>
+          </Grid>
+          <Timeline data={auditDetails?.body} />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default AuditTimeLine;
-
