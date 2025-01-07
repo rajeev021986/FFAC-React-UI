3aae9f5bd44808bedd79cd66c66e55fb6f02664c
GnanaprakashZmediostech <gnanaprakash@zmediostech.com>
2025-01-07 19:34:42 +0530

    valu echanhge

--- src/components/screen/code/vendor/vendorFilter.jsx
+++ src/components/screen/code/vendor/vendorFilter.jsx
@@ -8,115 +8,125 @@ import { updateVendorInput } from "../../../../store/freatures/vendorSlice";
 import SelectBox from "../../../common/SelectBox";
 
 export default function VendorFilterForm() {
-    const dispatch = useDispatch();
-    const inputs = useSelector((state) => state.vendor.formData);
+  const dispatch = useDispatch();
+  const inputs = useSelector((state) => state.vendor.formData);
 
-    const formik = useFormik({
-        initialValues: {
-            tinNo: inputs.tinNo || "",
-            vendorName: inputs.vendorName || "",
-            vrnNo: inputs.country || "",
-        },
-        onSubmit: (values) => {
-            dispatch(updateVendorInput(values));
-        },
-    });
-    const handleReset = () => {
-        formik.resetForm();
-        dispatch(
-            updateVendorInput({
-                tinNo: "",
-                vendorName: "",
-                vrnNo: "",
-            })
-        );
-    };
-    const StatusOptions = [{ value: "Active" }, { value: "InActive" }, { value: "New" }, { value: "Pending_Documents" }, { value: "Rejected" }];
-    const StatusOptionsDoc = [{ value: false, label: "Pending" }, { value: true, label: "Available" }];
-    return (
-        <div>
-            <Stack
-                spacing={4}
-                direction="column"
-                justifyContent="space-between"
-            >
-                <Stack direction="row" flexWrap="wrap" justifyContent="space-between" spacing={2} gap={2}>
-                    <InputBox
-                        label="Vendor Name"
-                        id="vendorName"
-                        value={formik.values.vendorName}
-                        onChange={formik.handleChange}
-                        sx={{ width: "48%" }}
-                    />
-                    <InputBox
-                        label="Tin No"
-                        id="tinNo"
-                        value={formik.values.tinNo}
-                        onChange={formik.handleChange}
-                        sx={{ width: "48%", marginLeft: "0px !important" }}
-                    />
-                    <InputBox
-                        label="Vrn No"
-                        id="vrnNo"
-                        value={formik.values.vrnNo}
-                        onChange={formik.handleChange}
-                        sx={{ width: "48%", marginLeft: "0px !important" }}
-                    />
-                    <div style={{ width: "48%", marginLeft: "0px" }}>
-                        <SelectBox
-                            label="Status"
-                            id="status"
-                            options={StatusOptions}
-                            value={formik.values.status}
-                            onChange={formik.handleChange}
-                            sx={{ marginLeft: "0px !important" }}
-                        />
-                    </div>
-                    <div style={{ width: "48%", marginLeft: "0px" }}>
-                        <SelectBox
-                            label="Doc Status"
-                            id="isDoc"
-                            options={StatusOptionsDoc}
-                            value={formik.values.doc_status}
-                            onChange={formik.handleChange}
-                            sx={{ marginLeft: "0px !important" }}
-                        />
-                    </div>
-                </Stack>
-
-                <Stack direction="row" spacing={3} justifyContent="flex-end">
-                    <Button
-                        color="primary"
-                        size="small"
-                        onClick={handleReset}
-                        sx={{
-                            borderRadius: '12px',
-                            padding: '6px 16px',
-                            textTransform: 'capitalize',
-                            backgroundColor: '#f5f5f5',
-                            color: '#333',
-                            '&:hover': {
-                                backgroundColor: '#e0e0e0',
-                            },
-                        }}
-                    >
-                        Reset
-                    </Button>
-                    <OutlinedButton
-                        color="primary"
-                        size="small"
-                        onClick={formik.handleSubmit}
-                        sx={{
-                            borderRadius: '12px',
-                            padding: '6px 16px',
-                            textTransform: 'capitalize',
-                        }}
-                    >
-                        Apply
-                    </OutlinedButton>
-                </Stack>
-            </Stack>
-
-        </div>
+  const formik = useFormik({
+    initialValues: {
+      tinNo: inputs.tinNo || "",
+      vendorName: inputs.vendorName || "",
+      vrnNo: inputs.country || "",
+    },
+    onSubmit: (values) => {
+      dispatch(updateVendorInput(values));
+    },
+  });
+  const handleReset = () => {
+    formik.resetForm();
+    dispatch(
+      updateVendorInput({
+        tinNo: "",
+        vendorName: "",
+        vrnNo: "",
+      })
     );
+  };
+  const StatusOptions = [
+    { label: "Active", value: 1 },
+    { label: "InActive", value: -2 },
+    { label: "New", value: 0 },
+    { label: "Pending Documents", value: 0 },
+    { label: "Rejected", value: -1 },
+  ];
+  const StatusOptionsDoc = [
+    { value: false, label: "Pending" },
+    { value: true, label: "Available" },
+  ];
+  return (
+    <div>
+      <Stack spacing={4} direction="column" justifyContent="space-between">
+        <Stack
+          direction="row"
+          flexWrap="wrap"
+          justifyContent="space-between"
+          spacing={2}
+          gap={2}
+        >
+          <InputBox
+            label="Vendor Name"
+            id="vendorName"
+            value={formik.values.vendorName}
+            onChange={formik.handleChange}
+            sx={{ width: "48%" }}
+          />
+          <InputBox
+            label="Tin No"
+            id="tinNo"
+            value={formik.values.tinNo}
+            onChange={formik.handleChange}
+            sx={{ width: "48%", marginLeft: "0px !important" }}
+          />
+          <InputBox
+            label="Vrn No"
+            id="vrnNo"
+            value={formik.values.vrnNo}
+            onChange={formik.handleChange}
+            sx={{ width: "48%", marginLeft: "0px !important" }}
+          />
+          <div style={{ width: "48%", marginLeft: "0px" }}>
+            <SelectBox
+              label="Status"
+              id="isApproved"
+              options={StatusOptions}
+              value={formik.values.status}
+              onChange={formik.handleChange}
+              sx={{ marginLeft: "0px !important" }}
+            />
+          </div>
+          <div style={{ width: "48%", marginLeft: "0px" }}>
+            <SelectBox
+              label="Doc Status"
+              id="isDoc"
+              options={StatusOptionsDoc}
+              value={formik.values.doc_status}
+              onChange={formik.handleChange}
+              sx={{ marginLeft: "0px !important" }}
+            />
+          </div>
+        </Stack>
+
+        <Stack direction="row" spacing={3} justifyContent="flex-end">
+          <Button
+            color="primary"
+            size="small"
+            onClick={handleReset}
+            sx={{
+              borderRadius: "12px",
+              padding: "6px 16px",
+              textTransform: "capitalize",
+              backgroundColor: "#f5f5f5",
+              color: "#333",
+              "&:hover": {
+                backgroundColor: "#e0e0e0",
+              },
+            }}
+          >
+            Reset
+          </Button>
+          <OutlinedButton
+            color="primary"
+            size="small"
+            onClick={formik.handleSubmit}
+            sx={{
+              borderRadius: "12px",
+              padding: "6px 16px",
+              textTransform: "capitalize",
+            }}
+          >
+            Apply
+          </OutlinedButton>
+        </Stack>
+      </Stack>
+    </div>
+  );
 }
