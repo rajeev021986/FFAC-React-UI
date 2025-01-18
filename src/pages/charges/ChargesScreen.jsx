48c624aa8849023493c2d0763782176ebcf9e755
GnanaprakashZmediostech <gnanaprakash@zmediostech.com>
2025-01-18 16:13:40 +0530

    speed dial theme cahnge

--- src/pages/charges/ChargesScreen.jsx
+++ src/pages/charges/ChargesScreen.jsx
@@ -229,18 +229,12 @@ export function ChargesScreen({ page }) {
                     tooltipTitle=""
                     sx={{
                       display: "flex",
-                      // width: "150px",
                       justifyContent: "center",
                       alignItems: "center",
                       padding: 2,
                       borderRadius: 1,
-                      backgroundColor: "#f0f0f0",
-                      color: "black",
                       boxShadow: 3,
                       borderRadius: "20px 19px 19px 20px",
-                      "&:hover": {
-                        backgroundColor: "#e0e0e0",
-                      },
                       width: 72,
                       minWidth: 92,
                       "& .MuiSvgIcon-root": {
