e2c3f5a9052c6aebf6c91e2b59fd8fdf41ca42c2
GnanaprakashZmediostech <gnanaprakash@zmediostech.com>
2025-01-21 20:35:07 +0530

    changes in chips

--- src/components/common/Filter/InputWithChips.jsx
+++ src/components/common/Filter/InputWithChips.jsx
@@ -46,6 +46,48 @@ const Chips = {
   vrnNo: {
     chipLabel: "VRN NO",
   },
+  icdCode: {
+    chipLabel: "ICD Code",
+  },
+  icdName: {
+    chipLabel: "ICD Name",
+  },
+  newPortName: {
+    chipLabel: "Port Name",
+  },
+  country: {
+    chipLabel: "Country",
+  },
+  vessel: {
+    chipLabel: "Vessel",
+  },
+  lname: {
+    chipLabel: "Line Name",
+  },
+  vname: {
+    chipLabel: "Vessel Name",
+  },
+  currency: {
+    chipLabel: "Vessel Name",
+  },
+  usdExchange: {
+    chipLabel: "USD Exchange",
+  },
+  chargeName: {
+    chipLabel: "Charge Name",
+  },
+  chargeCode: {
+    chipLabel: "Charge Code",
+  },
+  city: {
+    chipLabel: "City",
+  },
+  consigneeName: {
+    chipLabel: "Consignee Name",
+  },
+  name: {
+    chipLabel: "Name",
+  },
   statusCode: {
     chipLabel: "Status",
     chipvalues: [
