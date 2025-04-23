fc425a4ef186db49f74cbe6670ed1d717f9f7fc6
anjalibisht <anjali.bisht@zmediostech.com>
2025-04-23 17:07:04 +0530

    added breadcrumb

--- src/components/common/Breadcrumb.jsx
+++ src/components/common/Breadcrumb.jsx
@@ -67,12 +67,11 @@ const Routes = {
   editBond: { label: "Edit Bond", disabled: false },
 
   documentation: { label: "Documentation", disabled: true },
-  job: { label: "Job Entries", disabled: false },
-
+  jobEntry: { label: "Job Entries", disabled: false },
   newEntry: { label: "Add New Entry", disabled: true },
   editJobEntry: { label: "Edit Job Entry", disabled: false },
-  "job-approve": { label: "Approve New File", disabled: false },
-  update: { label: "Update Job", disabled: false },
+  approveJobfile: { label: "Approve New File", disabled: false },
+  updateJob: { label: "Update Job", disabled: false },
 };
 
 const ThemedBreadcrumb = () => {
