4ba8834d0069ca38ac530b8d6216431d79afa137
majidkhan372 <majid@zmediostech.com>
2025-05-14 13:38:35 -0400

    fix dropdown

--- src/pages/JobEntry/JobEntryForm.jsx
+++ src/pages/JobEntry/JobEntryForm.jsx
@@ -37,7 +37,6 @@ import UploadFile from "../../components/UploadFile";
 import { menuConfigUrl } from "../../store/menuConfigUrl";
 import AuditTimeLine from "../../components/AuditTimeLine";
 import ApiManager from "../../services/ApiManager";
-import SelectShipment from "../../components/common/SelectShipment";
 
 export default function JobEntryForm({
   initialValues,
@@ -318,7 +317,7 @@ export default function JobEntryForm({
         ...jobSettingData?.body,
       });
     }
-  }, [optionsSettingsData, customerSettingsData,jobSettingData]);
+  }, [optionsSettingsData, customerSettingsData, jobSettingData]);
 
   const handleApproveRequest = async () => {
     setRejectError(false);
@@ -556,7 +555,7 @@ export default function JobEntryForm({
           <TabPanel value="1">
             <Grid container rowSpacing={2} columnSpacing={3}>
               <Grid item xs={12} sm={6} md={4} lg={2} xl={2}>
-                <SelectShipment
+                <SelectBox
                   label="Shipment Type*"
                   id="shipmentType"
                   options={optionsSettingsData?.body?.shipmentType}
@@ -568,6 +567,7 @@ export default function JobEntryForm({
                   inputRef={shipmentTypeRef}
                 />
               </Grid>
+
               <Grid item xs={12} sm={6} md={4} lg={2} xl={2}>
                 <SelectBox
                   label="Move Type*"
