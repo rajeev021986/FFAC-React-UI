548481cd0ebad5d13f3744b22eeb7709a04c8699
majidkhan372 <majid@zmediostech.com>
2025-05-15 17:26:43 -0400

    added cancel api

---
+++ src/pages/setting/Vat-HoldingTax/Columns.js
@@ -0,0 +1,29 @@
+export const TAX_COLUMNS = [
+  {
+    flex: 1,
+    field: "id",
+    headerName: "ID",
+    width: 110,
+    headerAlign: "center",
+    align: "center",
+    editable: false,
+  },
+  {
+    flex: 1,
+    field: "value",
+    headerName: "Value",
+    width: 110,
+    headerAlign: "center",
+    align: "center",
+    editable: false,
+  },
+  {
+    flex: 1,
+    field: "action",
+    headerName: "Action",
+    width: 110,
+    headerAlign: "center",
+    align: "center",
+    editable: false,
+  },
+];
