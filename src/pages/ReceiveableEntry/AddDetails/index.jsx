0bc70661181df38a2ca578b772c90616317190fe
majidkhan372 <majid@zmediostech.com>
2025-05-28 15:03:36 +0530

    merge

--- src/pages/ReceiveableEntry/AddDetails/index.jsx
+++ src/pages/ReceiveableEntry/AddDetails/index.jsx
@@ -54,7 +54,7 @@ export default function ReceiveableEntryDetails({ page }) {
       type: data.type || "debit_note",
       containerTypeDTO: data.containerTypeDTO || [],
       costDetails: data.costDetails || [],
-      details: data?.receivableDetails || []
+      details: data?.receivableDetails || [],
     });
     const init = async () => {
       try {
