4ce9ac628bcfce8eff8519fad0b2e7c3effed5ac
anjalibisht <anjali.bisht@zmediostech.com>
2025-05-29 12:11:17 +0530

    added popup when changing invoice type

--- src/pages/ReceiveableEntry/SubSections/JobProfitAndLoss.jsx
+++ src/pages/ReceiveableEntry/SubSections/JobProfitAndLoss.jsx
@@ -1,13 +1,24 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AppBar, Box, Grid, Toolbar, Typography } from "@mui/material";
 
 // Components
 import InputBox from "../../../components/common/InputBox";
 import SelectBox from "../../../components/common/SelectBox";
 import CostDetails from "./CostDetails";
+import PopupAlert from "../../../components/common/Alert/PopupAlert";
 
 const JobProfitAndLoss = ({ formik }) => {
   const payableRef = useRef(null);
+  const [pendingInvoiceType, setPendingInvoiceType] = useState(null);
+  const [alertConfig, setAlertConfig] = useState({
+    open: false,
+    title: "",
+    message:
+      "All the Tax Invoice/Debit Note Details will be cleared if you change invoice type ",
+    severity: "info",
+    onConfirm: null,
+    onClose: () => setAlertConfig({ ...alertConfig, open: false }),
+  });
   useEffect(() => {
     if (payableRef?.current) {
       payableRef.current.focus();
@@ -185,14 +196,31 @@ const JobProfitAndLoss = ({ formik }) => {
                 error={formik.errors.type}
                 onChange={(e) => {
                   const value = e.target.value;
-                  if (formik.values.type !== value) {
-                    formik.setFieldValue("details", []);
-                  }
-                  formik.setFieldValue("type", value);
+                  if (formik.values.type === value) return;
+                  setPendingInvoiceType(value);
+                  setAlertConfig({
+                    open: true,
+                    title: "Are you sure you want to change Invoice Type?",
+                    message:
+                      "All the Tax Invoice/Debit Note Details will be cleared if you change invoice type.",
+                    severity: "info",
+                    confirmText: "Yes",
+                    onConfirm: () => {
+                      formik.setFieldValue("type", value);
+                      formik.setFieldValue("details", []);
+                      setAlertConfig((prev) => ({ ...prev, open: false }));
+                      setPendingInvoiceType(null);
+                    },
+                    onClose: () => {
+                      setAlertConfig((prev) => ({ ...prev, open: false }));
+                      setPendingInvoiceType(null);
+                    },
+                  });
                 }}
               />
             </Grid>
           </Grid>
+          {alertConfig.open && <PopupAlert alertConfig={alertConfig} />}
         </Box>
       </Box>
 
